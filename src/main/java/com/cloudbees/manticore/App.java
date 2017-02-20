4c36722a5e78e74596465c1894d9835726ece014
dariver <dariver@gmail.com>
2017-02-20 11:20:31 +0100

    failure

--- src/main/java/com/cloudbees/manticore/App.java
+++ src/main/java/com/cloudbees/manticore/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         String a = null;
-        System.out.println(a.toString());
+        System.out.println(a.toString())
         System.out.println( "Hello World!" );
         // TODO: this is a task
         // FIXME: this is another task
