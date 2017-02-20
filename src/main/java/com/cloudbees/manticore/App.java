30e225a0155e5897e25297ee683ba7564f4ffcda
dariver <dariver@gmail.com>
2017-02-20 10:27:52 +0100

    failure

--- src/main/java/com/cloudbees/manticore/App.java
+++ src/main/java/com/cloudbees/manticore/App.java
@@ -4,15 +4,15 @@ package com.cloudbees.manticore;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        String a = null;
+        String a = null
         System.out.println(a.toString());
         System.out.println( "Hello World!" );
         // TODO: this is a task
-        // FIXME: this is another task 
+        // FIXME: this is another task
         try {
             a = "Hello";
         } catch (Exception e) {}
